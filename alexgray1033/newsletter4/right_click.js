2681638602b9657b7d805a05afeb78c82f220d44
Yuhang <yuhangcui.yc@gmail.com>
2021-08-11 11:54:38 -0400

    newsletter 4 added

---
+++ alexgray1033/newsletter4/right_click.js
@@ -0,0 +1,72 @@
+// Tell the page to run our showMenu function on right click, and the hideMenu function on left click
+document.oncontextmenu = showMenu;
+document.onclick = hideMenu;
+var val = 50;
+var raving = false;
+var popping = false;
+
+function hideMenu() {
+    // Hide our flashy menu
+    document.getElementById("flashy-menu").style.visibility = "hidden";
+}
+
+function showMenu(clickEvent) {
+    // Don't display the default right click menu
+    clickEvent.preventDefault();
+    // Get our menu element
+    var menu = document.getElementById("flashy-menu");
+    // Hide menu if its already visible
+    if (menu.style.visibility == "visible") {
+        hideMenu();
+    } else {
+        // Show it at cursor location otherwise
+        menu.style.top = clickEvent.clientY + "px";
+        menu.style.left = clickEvent.clientX + "px";
+        menu.style.visibility = "visible";
+    }
+}
+
+document.getElementById('rainbow').onclick = function() {
+    // Turn off popping if it's on
+    if (document.getElementById("alex-content").classList.contains("poppy-rave")) {
+        document.getElementById("alex-content").classList.toggle("poppy-rave");
+        popping = false;
+    }
+    document.getElementById("alex-content").classList.toggle("rainbow-rave");
+    raving = !raving;
+}
+
+document.getElementById('pop').onclick = function() {
+    // Turn off rainbow if it's on
+    if (document.getElementById("alex-content").classList.contains("rainbow-rave")) {
+        document.getElementById("alex-content").classList.toggle("rainbow-rave");
+        raving = false;
+    }
+    document.getElementById("alex-content").classList.toggle("poppy-rave");
+    popping = !popping;
+}
+
+function lunacyTimer() {
+    if (raving) {
+        val += 1;
+        console.log("Current Lunacy: " + val);
+    } else if (popping) {
+        val -= 1;
+        console.log("Current Sanity: " + val);
+    }
+    if (val > 95) {
+        document.getElementById("value-name").innerHTML = "Maximum Lunacy";
+        document.getElementById("admin-login").style.visibility = "visible";
+        document.getElementById("lunacy-filter").style.visibility = "visible";
+        return;
+    }
+    if (val < 5) {
+        document.getElementById("value-name").innerHTML = "Minimum Sanity";
+        document.getElementById("admin-login").style.visibility = "visible";
+        document.getElementById("static-cover").style.visibility = "visible";
+        return;
+    }
+    setTimeout(function(){lunacyTimer()}, 100);
+}
+
+lunacyTimer();
