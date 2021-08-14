d6e7a6025ac268ede83df175d0fc5645d7255121
Yuhang <yuhangcui.yc@gmail.com>
2021-08-13 22:52:09 -0400

    newsletter 3 and 4 puzzle adjusted

--- alexgray1033/newsletter3/right_click.js
+++ alexgray1033/newsletter3/right_click.js
@@ -3,6 +3,7 @@ document.oncontextmenu = showMenu;
 document.onclick = hideMenu;
 var lunacy = 0;
 var showing = false;
+var stuck = false;
 
 function hideMenu() {
     // Hide our flashy menu
@@ -12,16 +13,18 @@ function hideMenu() {
 function showMenu(clickEvent) {
     // Don't display the default right click menu
     clickEvent.preventDefault();
-    // Get our menu element
-    var menu = document.getElementById("flashy-menu");
-    // Hide menu if its already visible
-    if (menu.style.visibility == "visible") {
-        hideMenu();
-    } else {
-        // Show it at cursor location otherwise
-        menu.style.top = clickEvent.clientY + "px";
-        menu.style.left = clickEvent.clientX + "px";
-        menu.style.visibility = "visible";
+    if (!stuck) {
+        // Get our menu element
+        var menu = document.getElementById("flashy-menu");
+        // Hide menu if its already visible
+        if (menu.style.visibility == "visible") {
+            hideMenu();
+        } else {
+            // Show it at cursor location otherwise
+            menu.style.top = clickEvent.clientY + "px";
+            menu.style.left = clickEvent.clientX + "px";
+            menu.style.visibility = "visible";
+        }
     }
 }
 
@@ -29,26 +32,41 @@ document.getElementById('rainbow').onclick = function() {
     // Turn off popping if it's on
     if (document.getElementById("alex-content").classList.contains("poppy-rave")) {
         document.getElementById("alex-content").classList.toggle("poppy-rave");
+        showing = false;
     }
     document.getElementById("alex-content").classList.toggle("rainbow-rave");
     showing = !showing;
+    if (showing) {
+        document.getElementById("lunacy-filter").style.visibility = "visible";
+    } else {
+        document.getElementById("lunacy-filter").style.visibility = "hidden";
+    }
 }
 
 document.getElementById('pop').onclick = function() {
     // Turn off rainbow if it's on
-    if (document.getElementById("alex-content").classList.contains("rainbow-rave")) {
+    if (showing) {
         document.getElementById("alex-content").classList.toggle("rainbow-rave");
+        document.getElementById("lunacy-filter").style.visibility = "hidden";
         showing = false;
     }
     document.getElementById("alex-content").classList.toggle("poppy-rave");
 }
 
 function lunacyTimer() {
+    // Make lunacy stuff more visible
     if (showing) {
         lunacy += 0.5;
         console.log("Current Lunacy: " + lunacy);
+        document.getElementById("lunacy-filter").style.opacity = (lunacy * 0.01).toString();
+        passLetters = document.getElementsByClassName("pass");
+        for (var i = 0; i < passLetters.length; i++) {
+            passLetters[i].style.filter = "grayscale(" + (lunacy + 40).toString() + "%) brightness(" + (100 - lunacy) + "%)";
+        }
     }
+    // Show warning after threshold
     if (lunacy > 95) {
+        stuck = true;
         document.getElementById("admin-login").style.visibility = "visible";
         document.getElementById("lunacy-filter").style.visibility = "visible";
         return;
--- alexgray1033/newsletter4/right_click.js
+++ alexgray1033/newsletter4/right_click.js
@@ -5,6 +5,7 @@ var val = 50;
 var raving = false;
 var popping = false;
 var maxSanity = 0;
+var stuck = false;
 
 function hideMenu() {
     // Hide our flashy menu
@@ -14,47 +15,69 @@ function hideMenu() {
 function showMenu(clickEvent) {
     // Don't display the default right click menu
     clickEvent.preventDefault();
-    // Get our menu element
-    var menu = document.getElementById("flashy-menu");
-    // Hide menu if its already visible
-    if (menu.style.visibility == "visible") {
-        hideMenu();
-    } else {
-        // Show it at cursor location otherwise
-        menu.style.top = clickEvent.clientY + "px";
-        menu.style.left = clickEvent.clientX + "px";
-        menu.style.visibility = "visible";
+    if (!stuck) {
+        // Get our menu element
+        var menu = document.getElementById("flashy-menu");
+        // Hide menu if its already visible
+        if (menu.style.visibility == "visible") {
+            hideMenu();
+        } else {
+            // Show it at cursor location otherwise
+            menu.style.top = clickEvent.clientY + "px";
+            menu.style.left = clickEvent.clientX + "px";
+            menu.style.visibility = "visible";
+        }
     }
 }
 
 document.getElementById('rainbow').onclick = function() {
     // Turn off popping if it's on
-    if (document.getElementById("alex-content").classList.contains("poppy-rave")) {
+    if (popping) {
         document.getElementById("alex-content").classList.toggle("poppy-rave");
         popping = false;
+        document.getElementById("static-cover").style.visibility = "hidden";
     }
     document.getElementById("alex-content").classList.toggle("rainbow-rave");
     raving = !raving;
+    if (raving) {
+        document.getElementById("lunacy-filter").style.visibility = "visible";
+    } else {
+        document.getElementById("lunacy-filter").style.visibility = "hidden";
+    }
 }
 
 document.getElementById('pop').onclick = function() {
     // Turn off rainbow if it's on
-    if (document.getElementById("alex-content").classList.contains("rainbow-rave")) {
+    if (raving) {
         document.getElementById("alex-content").classList.toggle("rainbow-rave");
+        document.getElementById("lunacy-filter").style.visibility = "hidden";
         raving = false;
     }
     document.getElementById("alex-content").classList.toggle("poppy-rave");
     popping = !popping;
+    if (popping) {
+        document.getElementById("static-cover").style.visibility = "visible";
+    } else {
+        document.getElementById("static-cover").style.visibility = "hidden";
+    }
 }
 
 function lunacyTimer() {
     if (raving) {
+        // Lunacy stuff become more visible
         val += 1;
         if (val > maxSanity) {
             maxSanity = val;
         }
         console.log("Current Lunacy: " + val);
+        document.getElementById("lunacy-filter").style.visibility = "visible";
+        document.getElementById("lunacy-filter").style.opacity = ((val - 50) * 0.02).toString();
+        passLetters = document.getElementsByClassName("pass");
+        for (var i = 0; i < passLetters.length; i++) {
+            passLetters[i].style.filter = "grayscale(" + (val + 40).toString() + "%) brightness(" + (100 - val) + "%)";
+        }
     } else if (popping) {
+        // Sanity stuff become more visible
         val -= 1;
         if (maxSanity - val > 60) {
             var wordList = document.getElementsByClassName("word");
@@ -62,15 +85,21 @@ function lunacyTimer() {
                 wordList[i].style.display = "inline";
             }
         }
+        document.getElementById("static-cover").style.visibility = "visible";
+        document.getElementById("static-cover").style.opacity = ((50 - val) * 0.02).toString();
         console.log("Current Sanity: " + val);
     }
+    // Show warning after lunaty and sanity thresholds
     if (val > 95) {
+        stuck = true;
+        document.getElementById("admin-warning").style.color = "var(--lunar-blue)";
         document.getElementById("value-name").innerHTML = "Maximum Lunacy";
         document.getElementById("admin-login").style.visibility = "visible";
         document.getElementById("lunacy-filter").style.visibility = "visible";
         return;
     }
     if (val < 5) {
+        stuck = true;
         document.getElementById("value-name").innerHTML = "Minimum Sanity";
         document.getElementById("admin-login").style.visibility = "visible";
         document.getElementById("static-cover").style.visibility = "visible";
