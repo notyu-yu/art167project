5216f6a7622b054a07771c7a088fa8101d0f860a
Yuhang <yuhangcui.yc@gmail.com>
2021-08-15 01:11:07 -0400

    added navigaiton bar and fixed code escape characters

--- alexgray1033/code_formatter.py
+++ alexgray1033/code_formatter.py
@@ -4,7 +4,7 @@ filename = sys.argv[1]
 
 with open(filename) as input_file:
     # Replace special characters with their escape character
-    formatted_string = input_file.read().replace('\t', '&emsp').replace('    ', '&emsp;').replace('<', '&lt').replace('>', '&gt').replace('\n', '<br>\n')
+    formatted_string = input_file.read().replace('\t', '&emsp;').replace('    ', '&emsp;').replace('<', '&lt;').replace('>', '&gt;').replace('\n', '<br>\n').replace('&', '&amp;')
     # Write the result to another file
     with open(filename+'.txt', 'w+') as output_file:
         output_file.write(formatted_string)
